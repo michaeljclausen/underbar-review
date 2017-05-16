2032242719c835810179a0b01373e884a33df069
Hack Reactor Student <student@hackreactor.com>
2017-05-16 11:34:41 -0700

    working on map

--- src/underbar.js
+++ src/underbar.js
@@ -117,6 +117,8 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    let results = [];
+    
   };
 
   /*
